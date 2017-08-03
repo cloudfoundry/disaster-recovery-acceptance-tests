56a7264e474e41e39b9a96882ee78952f4f9ab6a
Henry Stanley <hstanley@pivotal.io>
2017-08-03 15:39:13 +0100

    Rename test suite; add .idea to gitignore

--- acceptance_suite_test.go
+++ acceptance_suite_test.go
@@ -9,5 +9,5 @@ import (
 
 func TestPcfBackupAndRestoreAcceptanceTests(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "CfBackupAndRestoreAcceptanceTests Suite")
+	RunSpecs(t, "DisasterRecoveryAcceptanceTests Suite")
 }
