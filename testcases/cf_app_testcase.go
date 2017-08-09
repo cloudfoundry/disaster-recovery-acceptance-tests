f57fc09ebe59aabbbc4f70efb29b9a0748321347
Henry Stanley <hstanley@pivotal.io>
2017-08-09 14:01:06 +0100

    Reorder AfterRestore in cf app test case
    [#148986865]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

--- testcases/cf_app_testcase.go
+++ testcases/cf_app_testcase.go
@@ -36,16 +36,16 @@ func (tc *CfAppTestCase) AfterRestore(config Config) {
 	By("finding credentials for the deployment to restore")
 	RunCommandSuccessfully("cf login --skip-ssl-validation -a", config.DeploymentToRestore.ApiUrl, "-u", config.DeploymentToRestore.AdminUsername, "-p", config.DeploymentToRestore.AdminPassword)
 
+	By("verify orgs and spaces have been re-created")
+	RunCommandSuccessfully("cf org acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf target -o acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf space acceptance-test-space-" + tc.uniqueTestID)
+
 	By("verifying apps are back")
 	RunCommandSuccessfully("cf target -s acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
 	url := GetAppUrl("test_app_" + tc.uniqueTestID)
 
 	Eventually(StatusCode("https://"+url), 5*time.Minute, 5*time.Second).Should(Equal(200))
-
-	By("verify orgs and spaces have been re-created")
-	RunCommandSuccessfully("cf org acceptance-test-org-" + tc.uniqueTestID)
-	RunCommandSuccessfully("cf target -o acceptance-test-org-" + tc.uniqueTestID)
-	RunCommandSuccessfully("cf space acceptance-test-space-" + tc.uniqueTestID)
 }
 
 func (tc *CfAppTestCase) Cleanup(config Config) {
