9297a368277ee042f26a2819f255f5fd2ab9093e
Dennis Leon <dleon@pivotal.io>
2017-10-11 12:12:44 -0400

    Networking DRAT uses CLI commands, not plugin commands
    [#149968828]
    
    Signed-off-by: Joachim Valdez <valdezj@us.ibm.com>

--- testcases/cf_networking_testcase.go
+++ testcases/cf_networking_testcase.go
@@ -28,19 +28,19 @@ func (tc *CfNetworkingTestCase) BeforeBackup(config Config) {
 	var testAppFixturePath = path.Join(CurrentTestDir(), "/../fixtures/test_app/")
 	testAppName := fmt.Sprintf("test_app_%s", tc.uniqueTestID)
 	RunCommandSuccessfully("cf push " + testAppName + " -p " + testAppFixturePath)
-	RunCommandSuccessfully(fmt.Sprintf("cf allow-access %s %s --port 8080 --protocol tcp", testAppName, testAppName))
+	RunCommandSuccessfully(fmt.Sprintf("cf add-network-policy %s --destination-app %s --port 8080 --protocol tcp", testAppName, testAppName))
 }
 
 func (tc *CfNetworkingTestCase) AfterBackup(config Config) {
 	testAppName := fmt.Sprintf("test_app_%s", tc.uniqueTestID)
-	RunCommandSuccessfully(fmt.Sprintf("cf remove-access %s %s --port 8080 --protocol tcp", testAppName, testAppName))
+	RunCommandSuccessfully(fmt.Sprintf("cf remove-network-policy %s --destination-app %s --port 8080 --protocol tcp", testAppName, testAppName))
 }
 
 func (tc *CfNetworkingTestCase) AfterRestore(config Config) {
 	By("finding credentials for the deployment to restore")
 	RunCommandSuccessfully("cf login --skip-ssl-validation -a", config.DeploymentToRestore.ApiUrl, "-u", config.DeploymentToRestore.AdminUsername, "-p", config.DeploymentToRestore.AdminPassword)
 	RunCommandSuccessfully("cf target -s acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
-	session := RunCommand(fmt.Sprintf("cf list-access"))
+	session := RunCommand(fmt.Sprintf("cf network-policies"))
 	testAppName := fmt.Sprintf("test_app_%s", tc.uniqueTestID)
 	Eventually(session.Out).Should(gbytes.Say(fmt.Sprintf(`%s.*%s.*tcp.*8080`, testAppName, testAppName)))
 }
