037c31e00f63d70ee689d5522034c5e1a4ca89d5
Rosie Bloxsom <rbloxsom@pivotal.io>
2017-10-23 13:28:39 +0100

    nfs testcase tidying
    [#152095190]
    
    Signed-off-by: Henry Stanley <hstanley@pivotal.io>

--- testcases/nfs_testcase.go
+++ testcases/nfs_testcase.go
@@ -2,10 +2,9 @@ package testcases
 
 import (
 	"fmt"
+
 	. "github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests/common"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"os"
 )
 
 type NFSTestCase struct {
@@ -24,30 +23,35 @@ func (tc *NFSTestCase) Name() string {
 }
 
 func (tc *NFSTestCase) BeforeBackup(config Config) {
-	By("nfs before backup")
-	cwd, err := os.Getwd()
-	Expect(err).NotTo(HaveOccurred())
-	fmt.Printf("-------->cwd %s", cwd)
-	RunCommandSuccessfully("cf login --skip-ssl-validation -a", config.DeploymentToBackup.ApiUrl, "-u", config.DeploymentToBackup.AdminUsername, "-p", config.DeploymentToBackup.AdminPassword)
+	By("creating an NFS service broker and service instance")
+
+	RunCommandSuccessfully("cf login --skip-ssl-validation -a", config.DeploymentToBackup.ApiUrl,
+		"-u", config.DeploymentToBackup.AdminUsername, "-p", config.DeploymentToBackup.AdminPassword)
 	RunCommandSuccessfully("cf create-org acceptance-test-org-" + tc.uniqueTestID)
-	RunCommandSuccessfully("cf create-space acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
-	RunCommandSuccessfully("cf target -s acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf create-space acceptance-test-space-" + tc.uniqueTestID +
+		" -o acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf target -s acceptance-test-space-" + tc.uniqueTestID +
+		" -o acceptance-test-org-" + tc.uniqueTestID)
 	RunCommandSuccessfully("cf push dratsApp --docker-image docker/httpd --no-start")
 
 	if config.DeploymentToBackup.NFSBrokerUser != "" {
-		RunCommandSuccessfully("cf create-service-broker " + "nfsbroker-drats-" + tc.uniqueTestID + " " + config.DeploymentToBackup.NFSBrokerUser + " " + config.DeploymentToBackup.NFSBrokerPassword + " " + config.DeploymentToBackup.NFSBrokerUrl)
+		RunCommandSuccessfully("cf create-service-broker " + "nfsbroker-drats-" + tc.uniqueTestID + " " +
+			config.DeploymentToBackup.NFSBrokerUser + " " + config.DeploymentToBackup.NFSBrokerPassword + " " +
+			config.DeploymentToBackup.NFSBrokerUrl)
 	}
 	RunCommandSuccessfully("cf enable-service-access " + config.DeploymentToBackup.NFSServiceName)
-	RunCommandSuccessfully("cf create-service " + config.DeploymentToBackup.NFSServiceName + " " + config.DeploymentToBackup.NFSPlanName + " " + tc.instanceName + " -c " + `'{"share":"someserver.someplace.com/someshare"}'`)
+	RunCommandSuccessfully("cf create-service " + config.DeploymentToBackup.NFSServiceName + " " +
+		config.DeploymentToBackup.NFSPlanName + " " + tc.instanceName + " -c " +
+		`'{"share":"someserver.someplace.com/someshare"}'`)
 }
 
 func (tc *NFSTestCase) AfterBackup(config Config) {
-	By("nfs after backup")
+	By("deleting the NFS service instance after backup")
 	RunCommandSuccessfully("cf delete-service " + tc.instanceName + " -f")
 }
 
 func (tc *NFSTestCase) AfterRestore(config Config) {
-	By("nfs after backup")
+	By("re-binding the NFS service instance after restore")
 	RunCommandSuccessfully("cf bind-service dratsApp " + tc.instanceName + ` -c '{"uid":5000,"gid":5000}'`)
 }
 
