8d10fe40d003697e2b864467c62000c411bd6efd
Henry Stanley <hstanley@pivotal.io>
2017-11-22 16:37:32 +0000

    suppress stderr for curl in uptime test
    [#153067041]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- testcases/uptime_testcase.go
+++ testcases/uptime_testcase.go
@@ -79,7 +79,7 @@ func checkApiGoesDown(apiUrl string) (chan<- bool, <-chan bool) {
 				valueApiWasDown <- apiWasDown
 				return
 			case <-tickerChannel:
-				if RunCommand("curl", "-k", "--fail", apiUrl, "> /dev/null").ExitCode() == CURL_ERROR_FOR_404 {
+				if RunCommand("curl", "-k", "--fail", apiUrl, " 2>/dev/null > /dev/null").ExitCode() == CURL_ERROR_FOR_404 {
 					apiWasDown = true
 					ticker.Stop()
 				}
