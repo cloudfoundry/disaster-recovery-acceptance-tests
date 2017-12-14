796399345dc2b4b58b970400abfa4a26deb5e0fe
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-12-14 15:43:56 +0000

    Remove unused function
    [#152434450]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

--- testcases/testcase_helper.go
+++ testcases/testcase_helper.go
@@ -1,7 +1,6 @@
 package testcases
 
 import (
-	"fmt"
 	"github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests/runner"
 )
 
@@ -15,15 +14,4 @@ func OpenSourceTestCases() []runner.TestCase {
 		NewNFSTestCases(),
 		NewCfCredhubSSITestCase(),
 	}
-}
-
-func OpenSourceTestCasesWithFocus(suiteName string) []runner.TestCase {
-	for _, tc := range OpenSourceTestCases() {
-		if tc.Name() == suiteName {
-			fmt.Printf("Test focused: running %s test case\n", suiteName)
-			return []runner.TestCase{tc}
-		}
-	}
-
-	panic(fmt.Sprintf("Unable to find test case with name %s\n", suiteName))
 }
