2f74b01a868b9b1b0e25c61ac327d6f1c4836168
Emmanouil Kiagias <ekiagias@pivotal.io>
2018-05-21 14:23:20 +0100

    Revert "Merge pull request #32 from mdelillo/credhub-backup"
    This reverts commit c573a303d71c70bedc3374da09c44f2c7e7bf6df, reversing
    changes made to 4a29d09ebe4d6cfc39436fbfd46b67ebfe0f6b9d.
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- testcases/credhub_instance_creds_testcase.go
+++ testcases/credhub_instance_creds_testcase.go
@@ -1,32 +1,37 @@
 package testcases
 
 import (
-	"encoding/json"
-	"net/http"
 	"path"
+
+	"strings"
+
+	"io/ioutil"
 
 	. "github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests/runner"
 	. "github.com/onsi/gomega"
 )
 
 type CfCredhubSSITestCase struct {
-	uniqueTestID string
-	name         string
-	appName      string
-	appURL       string
+	uniqueTestID  string
+	name          string
+	appName       string
+	secondAppName string
+	brokerName    string
+	svcName       string
+	svcInstance   string
 }
 
 func NewCfCredhubSSITestCase() *CfCredhubSSITestCase {
 	id := RandomStringNumber()
 	return &CfCredhubSSITestCase{
-		uniqueTestID: id,
-		name:         "cf-credhub",
-		appName:      "app" + id,
+		uniqueTestID:  id,
+		name:          "cf-credhub",
+		svcName:       "service" + id,
+		svcInstance:   "instance" + id,
+		brokerName:    "broker" + id,
+		appName:       "app" + id,
+		secondAppName: "second-app" + id,
 	}
-}
-
-var listResponse struct {
-	Credentials []struct{}
 }
 
 func (tc *CfCredhubSSITestCase) Name() string {
@@ -36,37 +41,71 @@ func (tc *CfCredhubSSITestCase) Name() string {
 func (tc *CfCredhubSSITestCase) BeforeBackup(config Config) {
 	RunCommandSuccessfully("cf api --skip-ssl-validation", config.CloudFoundryConfig.ApiUrl)
 	RunCommandSuccessfully("cf auth", config.CloudFoundryConfig.AdminUsername, config.CloudFoundryConfig.AdminPassword)
+	cmdResponse := RunCommandSuccessfully("cf running-environment-variable-group").Out.Contents()
+	if !strings.Contains(string(cmdResponse), "CREDHUB_API") {
+		RunCommandSuccessfully("cf set-running-environment-variable-group '{ \"CREDHUB_API\": \"https://credhub.service.cf.internal:8844\"}'")
+	}
 	RunCommandSuccessfully("cf create-org acceptance-test-org-" + tc.uniqueTestID)
 	RunCommandSuccessfully("cf create-space acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
 	RunCommandSuccessfully("cf target -s acceptance-test-space-" + tc.uniqueTestID + " -o acceptance-test-org-" + tc.uniqueTestID)
+	var testBrokerPath = path.Join(CurrentTestDir(), "/../fixtures/credhub_service_broker/")
 
-	var testAppPath = path.Join(CurrentTestDir(), "/../fixtures/credhub-test-app")
-	RunCommandSuccessfully("cf push " + tc.appName + " -p " + testAppPath + " -f " + testAppPath + "/manifest.yml")
+	RunCommandSuccessfully("cf push " + tc.brokerName + " -p " + testBrokerPath + " -f " + testBrokerPath + "/manifest.yml" + " -b go_buildpack" + " -n " + tc.brokerName)
+	RunCommandSuccessfully("cf set-env " + tc.brokerName + " SERVICE_NAME " + tc.svcName)
+	RunCommandSuccessfully("cf restart " + tc.brokerName)
 
-	tc.appURL = GetAppUrl(tc.appName)
-	appResponse := Get(tc.appURL + "/create")
-	Expect(appResponse.StatusCode).To(Equal(http.StatusCreated))
+	serviceUrl := GetAppUrl(tc.brokerName)
+	RunCommandSuccessfully("cf create-service-broker " + tc.brokerName + " " + config.CloudFoundryConfig.AdminUsername + " " + config.CloudFoundryConfig.AdminPassword + " https://" + serviceUrl)
+	RunCommandSuccessfully("cf enable-service-access " + tc.svcName + " -o " + "acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf create-service " + tc.svcName + " credhub-read-plan " + tc.svcInstance)
 
-	appResponse = Get(tc.appURL + "/list")
-	Expect(json.NewDecoder(appResponse.Body).Decode(&listResponse)).To(Succeed())
-	Expect(listResponse.Credentials).To(HaveLen(1))
+	var testAppPath = path.Join(CurrentTestDir(), "/../fixtures/credhub_enabled_app/credhub-enabled-app.jar")
+	RunCommandSuccessfully("cf push " + tc.appName + " -p " + testAppPath + " --no-start" + " -n " + tc.appName)
+	RunCommandSuccessfully("cf set-env " + tc.appName + " SERVICE_NAME " + tc.svcName)
+	RunCommandSuccessfully("cf bind-service " + tc.appName + " " + tc.svcInstance)
+	RunCommandSuccessfully("cf start " + tc.appName)
+
+	appUrl := GetAppUrl(tc.appName)
+	appResponse := GetWithRetries(appUrl+"/test", 5)
+	appResponseBody, err := ioutil.ReadAll(appResponse.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(appResponseBody).To(ContainSubstring("pinkyPie"))
+	Expect(appResponseBody).To(ContainSubstring("rainbowDash"))
+
+	RunCommandSuccessfully("cf push " + tc.secondAppName + " -p " + testAppPath + " -n " + tc.secondAppName)
 }
 
 func (tc *CfCredhubSSITestCase) AfterBackup(config Config) {
-	appResponse := Get(tc.appURL + "/create")
-	Expect(appResponse.StatusCode).To(Equal(http.StatusCreated))
+	//do another bind that restore will clobber
+	RunCommandSuccessfully("cf set-env " + tc.secondAppName + " SERVICE_NAME " + tc.svcName)
+	RunCommandSuccessfully("cf bind-service " + tc.secondAppName + " " + tc.svcInstance)
+	RunCommandSuccessfully("cf restart " + tc.secondAppName)
 
-	appResponse = Get(tc.appURL + "/list")
-	Expect(json.NewDecoder(appResponse.Body).Decode(&listResponse)).To(Succeed())
-	Expect(listResponse.Credentials).To(HaveLen(2))
+	secondAppUrl := GetAppUrl(tc.secondAppName)
+	secondAppResponse := GetWithRetries(secondAppUrl+"/test", 5)
+	secondAppResponseBody, err := ioutil.ReadAll(secondAppResponse.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(secondAppResponseBody).To(ContainSubstring("pinkyPie"))
+	Expect(secondAppResponseBody).To(ContainSubstring("rainbowDash"))
 }
 
 func (tc *CfCredhubSSITestCase) AfterRestore(config Config) {
-	appResponse := Get(tc.appURL + "/list")
-	Expect(json.NewDecoder(appResponse.Body).Decode(&listResponse)).To(Succeed())
-	Expect(listResponse.Credentials).To(HaveLen(1))
+	appUrl := GetAppUrl(tc.appName)
+	appResponse := GetWithRetries(appUrl+"/test", 5)
+	appResponseBody, err := ioutil.ReadAll(appResponse.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(appResponseBody).To(ContainSubstring("pinkyPie"))
+	Expect(appResponseBody).To(ContainSubstring("rainbowDash"))
+
+	secondAppUrl := GetAppUrl(tc.secondAppName)
+	secondAppResponse := Get(secondAppUrl + "/test")
+	secondAppResponseBody, err := ioutil.ReadAll(secondAppResponse.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(secondAppResponseBody).NotTo(ContainSubstring("pinkyPie"))
+	Expect(secondAppResponseBody).NotTo(ContainSubstring("rainbowDash"))
 }
 
 func (tc *CfCredhubSSITestCase) Cleanup(config Config) {
 	RunCommandSuccessfully("cf delete-org -f acceptance-test-org-" + tc.uniqueTestID)
+	RunCommandSuccessfully("cf delete-service-broker -f " + tc.brokerName)
 }
