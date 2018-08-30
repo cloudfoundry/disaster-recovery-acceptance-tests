14ea58d7341cf817b0d99c600e6d110b6ef749a1
Dave Walter <dwalter@pivotal.io>
2018-08-30 07:00:30 -0700

    Use the NFSCreateServiceBroker flag
    - to control registration of the NFS service broker
    
    [#159975576](https://www.pivotaltracker.com/story/show/159975576)

--- testcases/nfs_testcase.go
+++ testcases/nfs_testcase.go
@@ -39,7 +39,7 @@ func (tc *NFSTestCase) BeforeBackup(config Config) {
 		" -o acceptance-test-org-" + tc.uniqueTestID)
 	RunCommandSuccessfully("cf push dratsApp --docker-image docker/httpd --no-start --random-route")
 
-	if config.CloudFoundryConfig.NFSBrokerPassword != "" {
+	if config.CloudFoundryConfig.NFSCreateServiceBroker {
 		RunCommandSuccessfully("cf create-service-broker nfsbroker-drats-" + tc.uniqueTestID + " " +
 			config.CloudFoundryConfig.NFSBrokerUser + " " + config.CloudFoundryConfig.NFSBrokerPassword + " " +
 			config.CloudFoundryConfig.NFSBrokerUrl)
@@ -65,7 +65,7 @@ func (tc *NFSTestCase) Cleanup(config Config) {
 	By("nfs cleanup")
 	RunCommandSuccessfully("cf delete-org -f acceptance-test-org-" + tc.uniqueTestID)
 
-	if config.CloudFoundryConfig.NFSBrokerPassword != "" {
+	if config.CloudFoundryConfig.NFSCreateServiceBroker {
 		RunCommandSuccessfully("cf delete-service-broker -f nfsbroker-drats-" + tc.uniqueTestID)
 	}
 }
