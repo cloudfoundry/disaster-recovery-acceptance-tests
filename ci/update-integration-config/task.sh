d3b1b528cb9b5459e4554e727e0ffc3ce6548452
Dave Walter <dwalter@pivotal.io>
2018-08-31 10:44:51 -0700

    Remove flag configuration params from update-integration-config CI tasks
    [#159975576](https://www.pivotaltracker.com/story/show/159975576)

--- ci/update-integration-config/task.sh
+++ ci/update-integration-config/task.sh
@@ -46,14 +46,6 @@ for config in "${configs[@]}"; do
   integration_config=$(echo "${integration_config}" | jq ".${config}=\"${!config}\"")
 done
 
-if [[ "${INCLUDE_NFS_BROKER_TESTCASE}" = "true" ]]; then
-  integration_config=$(echo "${integration_config}" | jq '."include_cf-nfsbroker"=true')
-
-  if [[ "${NFS_CREATE_SERVICE_BROKER}" = "true" ]]; then
-    integration_config=$(echo "${integration_config}" | jq '."nfs_create_service_broker"=true')
-  fi
-fi
-
 echo "${integration_config}" > "integration-configs/${INTEGRATION_CONFIG_FILE_PATH}"
 
 cp -Tr integration-configs updated-integration-configs
